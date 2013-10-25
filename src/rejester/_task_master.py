803418a0752533f5026763af528a770ff1703014
John R. Frank <jrf@diffeo.com>
2013-10-25 04:54:53 +0000

    basic TaskMaster and tests for it are now passing

--- src/rejester/_task_master.py
+++ src/rejester/_task_master.py
@@ -5,18 +5,56 @@ This software is released under an MIT/X11 open source license.
 
 Copyright 2012-2013 Diffeo, Inc.
 '''
+import time
+import logging
+from operator import itemgetter
 
+from rejester._logging import logger
 from rejester._registry import Registry
+from rejester.exceptions import ProgrammerError
+
 
 BUNDLES = 'bundles'
+NICE_LEVELS = 'NICE_LEVELS'
+WORK_SPECS = 'WORK_SPECS'
+WORK_UNITS_ = 'WORK_UNITS_'
+_FINISHED = '_FINISHED'
 
+class WorkUnit(object):
+    def __init__(self, registry, work_spec_name, key, data):
+        self.registry = registry
+        self.work_spec_name = work_spec_name
+        self.key = key
+        self.data = data
+        self._finished = False
+
+    def update(self, lease_time=300):
+        '''refresh the task lease by incrementing lease_time ahead of now
+        '''
+        if self._finished:
+            raise ProgrammerError('cannot update after finishing at task')
+        with self.registry.lock() as session:
+            session.update(
+                WORK_UNITS_ + self.work_spec_name,
+                {self.key: self.data}, 
+                {self.key: time.time() + lease_time})
+
+    def finish(self):
+        if self._finished:
+            return
+        with self.registry.lock() as session:
+            session.move(
+                WORK_UNITS_ + self.work_spec_name,
+                WORK_UNITS_ + self.work_spec_name + _FINISHED,
+                {self.key: self.data})
+        self._finished = True
 
 class TaskMaster(object):
     '''
     work_spec = dict(
         name = 
         desc = 
-        mingb = 
+        min_gb = 
         config = dict -- like we have in yaml files now
         work_unit = tuple -- like we emit with i_str now in streamcorpus.pipeline
     )
@@ -29,32 +67,79 @@ class TaskMaster(object):
         config['app_name'] = 'rejester'
         self.registry = Registry(config)
 
+    @classmethod
+    def validate_work_spec(cls, work_spec):
+        'raise ProgrammerError if work_spec is invalid'
+        if 'name' not in work_spec:
+            raise ProgrammerError('work_spec lacks "name"')
+        if 'min_gb' not in work_spec or \
+                not isinstance(work_spec['min_gb'], (float, int)):
+            raise ProgrammerError('work_spec["min_gb"] must be a number')
+
     def update_bundle(self, work_spec, work_units, nice=0):
         '''
 
         '''
+        self.validate_work_spec(work_spec)
         work_spec_name = work_spec['name']
         with self.registry.lock(atime=1000) as session:
-            session.update(
-                'nice_levels',
-                dict(work_spec_name=nice),
-            )
-            session.update(
-                'work_specs',
-                dict(work_spec_name=work_spec),
-            )
-            session.update(
-                'work_units_' + work_spec_name,
-                work_units,
-            )
+            session.update(NICE_LEVELS, {work_spec_name: nice})
+            session.update(WORK_SPECS,  {work_spec_name: work_spec})
+            
+            ## redis chokes on lua scripts with 
+            work_units = work_units.items()
+            window_size = 10**4
+            for i in xrange(0, len(work_units), window_size):
+                self.registry.re_acquire_lock(ltime=2000)
+                session.update(WORK_UNITS_ + work_spec_name, 
+                               dict(work_units[i: i + window_size]))
+                logger.debug('%d:%d', i, i + window_size)
 
     def nice(self, work_spec_name, nice):        
         with self.registry.lock(atime=1000) as session:
-            session.update(
-                'nice_levels',
-                dict(work_spec_name=nice),
-            )
+            session.update(NICE_LEVELS, dict(work_spec_name=nice))
 
-    def get_task(self, worker_class):
-        pass
+    def get_work(self, available_gb=None, lease_time=300):
+        '''obtain a WorkUnit instance based on available memory for the
+        worker process.  
 
+        :param available_gb: number of gigabytes of RAM available to
+        this worker
+
+        :param lease_time: how many seconds to lease a WorkUnit
+        '''
+        if not available_gb:
+            raise ProgrammerError('must always specify available_gb')
+
+        with self.registry.lock(atime=1000) as session:
+
+            ## should do something here, like a loop or greenlet with
+            ## timeout to avoid going past atime without taking action
+            
+            ## figure out which work_specs have low nice levels
+            nice_levels = session.pull(NICE_LEVELS)
+            nice_levels = nice_levels.items()
+            nice_levels.sort(key=itemgetter(1))
+
+            work_specs = session.pull(WORK_SPECS)
+            for work_spec_name, nice in nice_levels:
+
+                ## verify sufficient memory
+                if available_gb < work_specs[work_spec_name]['min_gb']:
+                    continue
+
+                logger.info('considering %s %s', work_spec_name, nice_levels)
+
+                ## try to get a task
+                _work_unit = session.getitem_reset(
+                    WORK_UNITS_ + work_spec_name,
+                    priority_max=time.time(),
+                    new_priority=time.time() + lease_time,
+                )
+
+                if _work_unit:
+                    return WorkUnit(
+                        self.registry, work_spec_name,
+                        _work_unit[0], _work_unit[1])
+
+            ## did not find work!
--- src/tests/rejester/test_task_master.py
+++ src/tests/rejester/test_task_master.py
@@ -2,13 +2,16 @@
 
 '''
 from __future__ import absolute_import
+from __future__ import division
 import os
 import sys
+import time
 import yaml
 import pytest
 import rejester
 from rejester import TaskMaster
 from rejester._logging import logger
+from rejester._task_master import WORK_UNITS_, _FINISHED
 
 from tests.rejester.make_namespace_string import make_namespace_string
 
@@ -34,17 +37,60 @@ def task_master(request):
     return task_master
 
 
-def test_task_master(task_master):
+def test_task_master_basic_interface(task_master):
     '''
     '''
     work_spec = dict(
         name = 'tbundle',
         desc = 'a test work bundle',
-        mingb = 8,
+        min_gb = 8,
         config = dict(many='', params=''),
-        work_unit = tuple(),
     )
 
-    task_master.update_bundle(work_spec, dict(foo={}, bar={}))
+    work_units = dict(foo={}, bar={})
+    task_master.update_bundle(work_spec, work_units)
 
+    assert task_master.registry.pull(WORK_UNITS_ + work_spec['name'])
+
+    ## check that we ccannot get a task with memory below min_gb
+    assert task_master.get_work(available_gb=3) == None
+
+    work_unit = task_master.get_work(available_gb=13)
+    assert work_unit.key in work_units
+
+    work_unit.data['status'] = 10
+    work_unit.update()
+
+    assert task_master.registry.pull(WORK_UNITS_ + work_spec['name'])[work_unit.key]['status'] == 10
+
+    work_unit.finish()
+    work_unit.finish()
+
+    assert task_master.registry.pull(WORK_UNITS_ + work_spec['name'] + _FINISHED)[work_unit.key]['status'] == 10   
     
+
+def test_task_master_throughput(task_master):
+    '''
+    '''
+    ## 1KB sized work_spec config
+    work_spec = dict(
+        name = 'tbundle',
+        desc = 'a test work bundle',
+        min_gb = 8,
+        config = dict(many=' ' * 2**10, params=''),
+    )
+
+    num_units = 10**6
+    work_units = {str(x): {str(x): ' ' * 30} for x in xrange(num_units)}
+
+    start = time.time()
+    task_master.update_bundle(work_spec, work_units)
+    elapsed = time.time() - start
+    logger.info('%d work_units pushed in %.1f seconds --> %.1f units/sec',
+                num_units, elapsed, num_units / elapsed)
+
+    assert len(task_master.registry.pull(WORK_UNITS_ + work_spec['name'])) == num_units
+
+    work_unit = task_master.get_work(available_gb=13)
+    assert work_unit.key in work_units
+
