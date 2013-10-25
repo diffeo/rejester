e12ae8398766be55ffa56cfe81f6b196a2151529
John R. Frank <jrf@diffeo.com>
2013-10-25 04:55:16 +0000

    making RejesterException base exception

--- src/rejester/exceptions.py
+++ src/rejester/exceptions.py
@@ -6,9 +6,24 @@ This software is released under an MIT/X11 open source license.
 Copyright 2012-2013 Diffeo, Inc.
 '''
 
-
-class EnvironmentError(Exception):
+class RejesterException(Exception):
+    'base exception for rejester package'
     pass
 
-class LockError(Exception):
+class EnvironmentError(RejesterException):
+    '''indicates that the registry lost a lock or experienced a similar
+failure that probably indicates a network or remote server failure'''
+    pass
+
+class LockError(RejesterException):
+    'attempt to get a lock exceeded acquire time (atime)'
+    pass
+
+class ProgrammerError(RejesterException):
+    pass
+
+class PriorityRangeEmpty(RejesterException):
+    '''
+    given the priority_min/max, no item is available to be returned
+    '''
     pass
