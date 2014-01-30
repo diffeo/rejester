50be08a837832c399f4d07279029085570ed5c95
David Maze <dmaze@diffeo.com>
2014-01-30 18:17:12 +0000

    Fix (up) rejester test_run.py

--- src/tests/rejester/test_run.py
+++ src/tests/rejester/test_run.py
@@ -5,35 +5,36 @@ import time
 import uuid
 import pytest
 import signal
-import shutil
 import psutil
 import subprocess
 from rejester._logging import logger
+from tests.rejester.make_namespace_string import make_namespace_string
 
-@pytest.fixture(scope='function')
-def tmp_dir(request):
-    tmp_dir = os.path.join('/tmp', str(uuid.uuid4()))
-    os.makedirs(tmp_dir)
+@pytest.fixture
+def rejester_cli_namespace(request):
+    namespace = make_namespace_string()
     def fin():
-        shutil.rmtree(tmp_dir, ignore_errors=True)
+        subprocess.call(['rejester', 'delete', namespace])
+        pass
     request.addfinalizer(fin)
-    return tmp_dir
+    return namespace
 
-def test_run(tmp_dir):
-    tmp_pid = os.path.join(tmp_dir, 'pid')
-    logging.info('pidfile=%r', tmp_pid)
+def test_run(tmpdir, rejester_cli_namespace):
+    namespace = rejester_cli_namespace
+    tmp_pid = str(tmpdir.join('pid'))
+    tmp_log = str(tmpdir.join('log'))
+    logger.info('pidfile=%r', tmp_pid)
     p = subprocess.Popen(
-        ['rejester', 'run_worker', 'test_namespace', '--pidfile', tmp_pid],
+        ['rejester', 'run_worker', namespace, '--pidfile', tmp_pid,
+         '--logpath', tmp_log],
         stderr=subprocess.PIPE,
         stdout=subprocess.PIPE,
     )
     max_time = 20
     start_time = time.time()
     elapsed = 0
-    print 'starting...'
-    print '%r exists = %r' % (tmp_pid, os.path.exists(tmp_pid))
     while elapsed < max_time:
-        print '%r exists = %r' % (tmp_pid, os.path.exists(tmp_pid))
+        logger.debug('{0} exists? {1}'.format(tmp_pid, os.path.exists(tmp_pid)))
         elapsed = time.time() - start_time
         out, err = p.communicate()
         print out
@@ -45,7 +46,7 @@ def test_run(tmp_dir):
     while elapsed < max_time:
         if os.path.exists(tmp_pid):
             break
-        print '%r not there yet, wait' % (tmp_pid,)
+        logger.debug('{0} not there yet, wait'.format(tmp_pid))
         time.sleep(0.2)  # wait a moment for daemon child to wake up and write pidfile
         elapsed = time.time() - start_time
     pid = int(open(tmp_pid).read())
@@ -56,8 +57,8 @@ def test_run(tmp_dir):
     while elapsed < max_time:
         if pid not in psutil.get_pid_list():
             break
-        logger.critical( '%.1f elapsed seconds, %d still alive' % (elapsed, pid))
+        logger.debug( '%.1f elapsed seconds, %d still alive' % (elapsed, pid))
         time.sleep(0.2)  # wait a moment for daemon child to exit
         elapsed = time.time() - start_time
     assert pid not in psutil.get_pid_list()
-    print 'killed', pid
+    logger.debug('killed {0}'.format(pid))
