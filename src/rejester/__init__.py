8675fccdcbdb6e4f0870cd666da9fe7dbd54c97e
David Maze <dmaze@diffeo.com>
2014-01-31 16:43:05 +0000

    Expose rejester.RejesterQueue() class

--- src/rejester/__init__.py
+++ src/rejester/__init__.py
@@ -3,9 +3,11 @@ http://github.com/diffeo/rejester
 
 This software is released under an MIT/X11 open source license.
 
-Copyright 2012-2013 Diffeo, Inc.
+Copyright 2012-2014 Diffeo, Inc.
 '''
 
 from _task_master import TaskMaster
+from _queue import RejesterQueue
 from _registry import Registry
 from _logging import reset_log_level
+import support
