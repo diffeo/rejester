0fbd5919e0755a2cc2155404e1bbcb6de9b70123
David Maze <dmaze@diffeo.com>
2014-08-05 16:08:57 +0000

    Rejester can find Redis from environment variables.
    $REDIS_PORT_6379_TCP_ADDR and $REDIS_PORT_6379_TCP_PORT will fill in
    registry_addresses if nothing else does.  This sounds verbose, but
    it's what Docker provides if you run a rejester application linked
    to a Redis container with --link something:redis.

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ setup(
     ],
     install_requires=[
         'dblogger >= 0.4.0',
-        'yakonfig >= 0.5.0',
+        'yakonfig >= 0.6.0',
         'gevent',
         'pyyaml',
         'redis',
